ccbf96cff0470bc00a182628a3d85caf0c725b27
Dennis-Dang <dennis.dang5521@gmail.com>
2023-05-06 23:31:46 -0700

    Implement ball bounce with north and south walls

--- pong/ball.py
+++ pong/ball.py
@@ -6,8 +6,10 @@ class Ball(Turtle):
     def __init__(self):
         super().__init__(shape="circle")
         self.color("white")
-        self.setheading(random.randint(91, 269))
+        self.setheading(random.randrange(0, 360, 40))
         self.penup()
 
     def move(self):
+        if self.ycor() <= -280 or self.ycor() >= 280:
+            self.setheading(-self.heading())
         self.forward(10)
