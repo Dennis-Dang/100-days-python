7b80d80ac15824ef85e619bccd7412795566a0bd
Dennis-Dang <dennis.dang5521@gmail.com>
2023-05-07 15:01:16 -0700

    Add player functionality: Class constructor with starting attributes, moving the turtle with event listener, returning turtle to starting position after reaching the end of the screen.

--- turtle crossing/player.py
+++ turtle crossing/player.py
@@ -1,7 +1,21 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__(shape="turtle")
+        self.penup()
+        # Face north
+        self.setheading(90)
+        self.color("green")
+        self.goto(STARTING_POSITION)
+
+    def up(self):
+        self.forward(MOVE_DISTANCE)
+
+    def reset_pos(self):
+        self.goto(STARTING_POSITION)
