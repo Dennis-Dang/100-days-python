eb44c7d9a93df1cc80366801ccebc73d0b728511
Dennis-Dang <dennis.dang5521@gmail.com>
2023-05-09 16:51:12 -0700

    Optimized code so that every letter is uppercased when user asked for input, instead of looping through each letter to uppercase them.

--- nato alphabet speller/main.py
+++ nato alphabet speller/main.py
@@ -14,12 +14,11 @@ nato_dict = {row.letter: row.code for (index, row) in nato_df.iterrows()}
 # Keep asking for user input until user enters "exit"
 loop = True
 while loop:
-    word = input("Enter a word to turn into NATO: ")
-    # Convert every letter into uppercase to later match with the keys of the NATO alphabet dictionary
-    upper_word = [letter.upper() for letter in word]
-    if upper_word != "EXIT":
+    word = input("Enter a word to turn into NATO: ").upper()
+    if word != "EXIT":
         # Use the NATO dictionary to convert each letter into their corresponding NATO alphabet code.
-        phonetic_word = [nato_dict[letter] for letter in upper_word if letter in nato_dict]
+        # Condition in dictionary comprehension is necessary in case user enters a character outside of NATO alphabet.
+        phonetic_word = [nato_dict[letter] for letter in word if letter in nato_dict]
         print(phonetic_word)
     else:
         loop = False
