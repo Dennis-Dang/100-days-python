b57635929cbda1badbc62aef58195bbe54b419e3
Dennis-Dang <dennis.dang5521@gmail.com>
2023-05-09 16:37:41 -0700

    Finish TODO 2: Create a list of phonetic NATO words from a word that user inputs.

--- nato alphabet speller/main.py
+++ nato alphabet speller/main.py
@@ -29,6 +29,15 @@ import pandas
 nato_df = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter: row.code for (index, row) in nato_df.iterrows()}
 
-print(nato_dict)
-# TODO: Create a list of the phonetic code words from a word that the user inputs.
-
+# print(nato_dict)
+# Create a list of the phonetic code words from a word that the user inputs.
+loop = True
+while loop:
+    word = input("Enter a word to turn into NATO: ")
+    upper_word = [letter.upper() for letter in word]
+    if upper_word != "EXIT":
+        phonetic_word = [nato_dict[letter] for letter in upper_word if letter in nato_dict]
+        print(phonetic_word)
+    else:
+        loop = False
+        print("Goodbye!")
