c813ce50e60c5b32eb0c3ce34045d2c1e6bb1e32
Dennis-Dang <dennis.dang5521@gmail.com>
2023-05-11 21:07:34 -0700

    In Password Manager project, add app UI controls.

--- Password Manager GUI/main.py
+++ Password Manager GUI/main.py
@@ -17,12 +17,37 @@ def get_canvas_size(event):
 window = Tk()
 window.title("Password Manager")
 window.config(padx=20, pady=20)
+window.resizable(False, False)
 
 canvas = Canvas(master=window, height=200, width=200)
 logo_img = PhotoImage(file="logo.png")
 image_item = canvas.create_image(100, 100, image=logo_img)
 canvas.grid(column=0, row=0)
-
 canvas.bind("<Configure>", get_canvas_size)
+
+control_frame = Frame(master=window)
+lbl_website = Label(master=control_frame, text="Website: ")
+lbl_username = Label(master=control_frame, text="Email/Username: ")
+ent_website = Entry(master=control_frame)
+ent_username = Entry(master=control_frame)
+lbl_password = Label(master=control_frame, text="Password: ")
+
+frame2 = Frame(master=control_frame)
+ent_password = Entry(master=frame2)
+btn_generate = Button(master=frame2, text="Generate Password")
+ent_password.grid(column=0, row=0, sticky="w", padx=(0, 50))
+btn_generate.grid(column=1, row=0, sticky="e")
+frame2.grid(column=1, row=2, sticky="w")
+
+lbl_website.grid(column=0, row=0, sticky="w")
+ent_website.grid(column=1, row=0, sticky="ew")
+lbl_username.grid(column=0, row=1, sticky="w")
+ent_username.grid(column=1, row=1, sticky="ew")
+lbl_password.grid(column=0, row=2, sticky="w")
+
+
+btn_add = Button(master=control_frame, text="Add")
+btn_add.grid(column=1, row=3, columnspan=2, sticky="ew")
+control_frame.grid(column=0, row=1)
 
 window.mainloop()
