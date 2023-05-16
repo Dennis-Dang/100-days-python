f51351f9e3959d13bb94770b49069e5892a54347
Dennis-Dang <dennis.dang5521@gmail.com>
2023-05-16 01:53:37 -0700

    In ISS Overhead Notifier, change distance calculation of ISS overhead. Uses the haversine formula, which is a comparably better approximation of distance on a spherical shape.

--- ISS Overhead Notifier/main.py
+++ ISS Overhead Notifier/main.py
@@ -1,18 +1,30 @@
 from dotenv import dotenv_values
 import requests
+from haversine import haversine, Unit
 import datetime as dt
 import smtplib
 
 config = dotenv_values(".env")
+# Client's Coordinate position
 LATITUDE = float(config["LATITUDE"])
 LONGITUDE = float(config["LONGITUDE"])
 
 ISS_API_ENDPOINT = "http://api.open-notify.org/iss-now.json"
 SUNSET_SUNRISE_API_ENDPOINT = "https://api.sunrise-sunset.org/json"
 
+# Viewable distance at a 40 degree height above the horizon.
+# This is approximated by taking the average altitude of the ISS, 253 miles.
+# flyover_distance = 253 / tan(40) ~ 301.51366
+flyover_distance = 301
+
 
 def is_overhead(iss_lat, iss_lon):
-    if abs(iss_lat - LATITUDE) <= 5 and abs(iss_lon - LONGITUDE) <= 5:
+    """Determines whether the ISS is within viewable range. Uses the haversine formula to calculate distances between
+    two points on a sphere. However since the Earth is slightly ellipsoidal, the calculation is accurate enough.
+    :param iss_lat ISS's latitude coordinate position
+    :param iss_lon ISS's longitude coordinate position
+    :returns True if the ISS is within viewable range, otherwise returns false."""
+    if haversine((iss_lat, iss_lon), (LATITUDE, LONGITUDE), unit=Unit.MILES) <= flyover_distance:
         return True
     else:
         return False
