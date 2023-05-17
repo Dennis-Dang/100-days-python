aea261bacfb450ce48d103c6235a3289fc634c74
Dennis-Dang <dennis.dang5521@gmail.com>
2023-05-17 03:56:45 -0700

    In Rain Alert app, implement API call and check if there will be any rain within the next 12 hours.

--- Rain Alert/main.py
+++ Rain Alert/main.py
@@ -14,5 +14,41 @@ response = requests.get("https://api.openweathermap.org/data/2.8/onecall", param
 response.raise_for_status()
 print(response)
 data = response.json()
-print(data)
+# Get the first 12 hours of weather data.
+hourly_weather = data["hourly"][:12]
 
+# Sample Weather Hour Response:
+# (The code above will filter weather data to have 12 of these)
+# {
+#     'dt': 1684317600,
+#     'temp': 62.19,
+#     'feels_like': 61.39,
+#     'pressure': 1013,
+#     'humidity': 70,
+#     'dew_point': 52.3,
+#     'uvi': 0,
+#     'clouds': 0,
+#     'visibility': 10000,
+#     'wind_speed': 3.58,
+#     'wind_deg': 166,
+#     'wind_gust': 4.52,
+#     'weather': [
+#       {
+#         'id': 800,
+#         'main': 'Clear',
+#         'description': 'clear sky',
+#         'icon': '01n'
+#       }
+#     ],
+#     'pop': 0
+# }
+rain = False
+for hour in hourly_weather:
+    for weather_condition in hour["weather"]:
+        # Codes below 700 justify bringing an umbrella.
+        # https://openweathermap.org/weather-conditions#Weather-Condition-Codes-2
+        if weather_condition["id"] < 700:
+            rain = True
+
+if rain:
+    print("Bring an umbrella.")
