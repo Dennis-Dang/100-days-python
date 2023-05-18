1bb8564c6c04c1c7fd62dbf54069d769c5ba130f
Dennis-Dang <dennis.dang5521@gmail.com>
2023-05-17 18:09:46 -0700

    In Stock News Alert app, implement checking the closing stock prices for the previous 2 days to see if there was any major percentage difference.

--- Stock News Alert/main.py
+++ Stock News Alert/main.py
@@ -1,9 +1,30 @@
+from dotenv import dotenv_values
+import requests
+
+config = dotenv_values(".env")
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
 ## STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+parameters = {
+    "function": "TIME_SERIES_DAILY_ADJUSTED",
+    "symbol": "GOOGL",
+    "apikey": config["ALPHAV_API_KEY"]
+}
+response = requests.get("https://www.alphavantage.co/query", params=parameters)
+response.raise_for_status()
+stock_data = response.json()["Time Series (Daily)"]
+daily_stock = [value for (key, value) in stock_data.items()]
+yesterdays_closing = daily_stock[1]['4. close']
+day_before_yesterdays_closing = daily_stock[2]['4. close']
 
+
+closing_diff = abs(float(yesterdays_closing) - float(day_before_yesterdays_closing))
+delta_percent = (closing_diff / float(yesterdays_closing)) * 100
+
+if delta_percent > 2:
+    print("Get News")
 ## STEP 2: Use https://newsapi.org
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
 
