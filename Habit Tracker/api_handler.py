b58e4f5e4152106a308bd1b5818ab42b913f6bcf
Dennis-Dang <dennis.dang5521@gmail.com>
2023-05-20 01:27:28 -0700

    In Habit Tracker app, add endpoint attribute. Also, add regex for input validation for inputting tokens and username.

--- Habit Tracker/api_handler.py
+++ Habit Tracker/api_handler.py
@@ -1,12 +1,13 @@
 import requests
 from dotenv import dotenv_values
+import re
 
 config = dotenv_values('.env')
 
 
 class FitTracker:
     def __init__(self):
-        self.endpoint = config["PIXELA_ENDPOINT"]
+        self.endpoint = "https://pixe.la"
         self.token = config["TOKEN"]
         self.username = config["USERNAME"]
         if self.token and self.username:
@@ -18,7 +19,15 @@ class FitTracker:
 
     def create_user(self):
         token = input("Type in your token: ")
-        username = input("Type in your username: ")
+        while not re.match(r"^[\w\D]{8,128}$", token):
+            print("Token must alphanumeric (a-z)(A-Z)(0-9)")
+            token = input("Type in your token: ")
+        username = input("Type in your username")
+        while not re.match("^[a-z][a-z0-9]{1,32}$", username):
+            print("Username must be within 32 characters in length, "
+                  "start with a letter, followed by alphanumeric characters.")
+            username = input("Type in your username: ")
+
         pixela_parameters = {
             "token": token,
             "username": username,
@@ -28,8 +37,9 @@ class FitTracker:
         response = requests.post(f"{self.endpoint}/v1/users", json=pixela_parameters)
         response.raise_for_status()
         print(response.json())
+        return response.json()
 
-    def create_graph(self, name: str):
+    def create_graph(self):
         graph_format = {
             "id": "graph1",
             "name": "Exercise Tracker",
@@ -38,7 +48,7 @@ class FitTracker:
             "color": "sora",
             "timezone": "America/Los_Angeles"
         }
-        response = requests.post(f"{self.endpoint}/v1/users/{config['USERNAME']}/graphs", json=graph_format,
+        response = requests.post(f"{self.endpoint}/v1/users/{self.username}/graphs", json=graph_format,
                                  headers=self.header)
         response.raise_for_status()
         graph_data = response.json()
@@ -50,7 +60,7 @@ class FitTracker:
             "quantity": quantity
         }
 
-        response = requests.post(f"{self.endpoint}/v1/users/{config['USERNAME']}/graphs/{graph_id}",
+        response = requests.post(f"{self.endpoint}/v1/users/{self.username}/graphs/{graph_id}",
                                  json=pixel_parameters,
                                  headers=self.header)
         response.raise_for_status()
@@ -61,14 +71,14 @@ class FitTracker:
             "quantity": quantity
         }
 
-        response = requests.put(f"{self.endpoint}/v1/users/{config['USERNAME']}/graphs/{graph_id}/{date}",
+        response = requests.put(f"{self.endpoint}/v1/users/{self.username}/graphs/{graph_id}/{date}",
                                 json=pixel_parameters,
                                 headers=self.header)
         response.raise_for_status()
         print(response.json())
 
     def delete_pixel(self, graph_id: str, date: str):
-        response = requests.delete(f"{self.endpoint}/v1/users/{config['USERNAME']}/graphs/{graph_id}/{date}",
+        response = requests.delete(f"{self.endpoint}/v1/users/{self.username}/graphs/{graph_id}/{date}",
                                    headers=self.header)
         response.raise_for_status()
         print(response.json())
