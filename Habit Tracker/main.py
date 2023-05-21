75654348d3f5a37891dfcf5401044d876458a36d
Dennis-Dang <dennis.dang5521@gmail.com>
2023-05-20 17:13:29 -0700

    In Habit Tracker app, compartmentalize date input for better modularity for other api functions.

--- Habit Tracker/main.py
+++ Habit Tracker/main.py
@@ -15,36 +15,39 @@ Delete: Delete an entry.
 Settings: Configure account settings.
 '''
 
+
+def get_date() -> str or None:
+    print("Enter the date interest for the pixel. (YYYY-MM-DD)\n"
+          "Or press Enter to submit as today's date.")
+    str_date = pyip.inputDatetime('Enter Date ', formats=('%Y-%m-%d',),
+                                  blank=True)
+    if not str_date:
+        date = dt.datetime.today()
+    else:
+        date = str_date.date()
+        if date > dt.datetime.today().date():
+            print(f"You are making an entry into the future. I cannot do that. "
+                  f"Enter a date before or on {dt.datetime.today().strftime('%Y-%m-%d')}. Returning to menu.")
+            return None
+
+    return date.strftime('%Y-%m-%d')
+
+
 print("Welcome to the Habit Tracker.")
 to_do = pyip.inputMenu(['add/modify', 'delete', 'settings', 'idk'], "What would you like to do?\n", numbered=True)
 while 'exit' != to_do:
     if to_do == 'idk':
         print(commands)
     elif to_do == 'add/modify':
-        print("Enter the date you want to add the entry for. (YYYY-MM-DD)\n"
-              "Or press Enter to submit as today's date.")
-        str_date = pyip.inputDatetime('Enter Date ', formats=('%Y-%m-%d', '%Y/%m/%d', '%Y.%m.%d', '%Y %m %d'),
-                                      blank=True)
-        date = None
-        if not str_date:
-            date = dt.datetime.today()
-        else:
-            try:
-                date = dt.datetime.strptime(str_date, "%Y-%m-%d").date()
-                if date > dt.datetime.today().date():
-                    print(f"You are making an entry into the future. I cannot do that. "
-                          f"Enter a date before {dt.datetime.today().strftime('%Y-%m-%d')}. Returning to menu.")
-                    date = None
-            except ValueError:
-                print("Invalid date. Date must be formatted YYYY-MM-DD")
+        pixel_date = get_date()
 
-        if date:
-            quantity = pyip.inputNum(f"How many sit-ups did you do on {date.strftime('%Y-%m-%d')}? ")
-            confirm = pyip.inputYesNo(f"You've done {quantity} sit-ups on {date.strftime('%Y-%m-%d')}. Confirm? (Y/N) ")
+        if pixel_date:
+            quantity = pyip.inputNum(f"How many sit-ups did you do on {pixel_date}? ")
+            confirm = pyip.inputYesNo(f"You've done {quantity} sit-ups on {pixel_date}. Confirm? (Y/N) ")
             if confirm == 'yes':
                 if tracker.ids['GRAPH_ID']:
                     try:
-                        tracker.add_pixel(str(quantity), date.strftime("%Y%m%d"))
+                        tracker.add_pixel(str(quantity), pixel_date)
                     except Exception as e:
                         print(f"{type(e).__name__} at line {e.__traceback__.tb_lineno} of {__file__}: {e}")
                     else:
