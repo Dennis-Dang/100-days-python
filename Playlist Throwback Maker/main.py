082406cf2bc544e51819b7d1c938b44a5e2aac10
Dennis-Dang <dennis.dang5521@gmail.com>
2023-05-25 06:17:20 -0700

    In Playlist Throwback Maker, use Spotipy (Spotify API driver) to search for track URIs for each song.

--- Playlist Throwback Maker/main.py
+++ Playlist Throwback Maker/main.py
@@ -4,29 +4,30 @@ import requests
 from bs4 import BeautifulSoup
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+from time import sleep
 from dotenv import dotenv_values
 config = dotenv_values('.env')
 CLIENT_ID = config["CLIENT_ID"]
 CLIENT_SECRET = config["CLIENT_SECRET"]
 
-# str_date = pyip.inputDatetime("What year do you want to travel to? Type in the date in this format YYYY-MM-DD: ",
-#                               formats=('%Y-%m-%d',))
-# date = str_date.date()
-#
-# while date > dt.datetime.today().date():
-#     print(f"This is a date in the future, songs are not created yet for this date!\n"
-#           f"Enter a date before or on {dt.datetime.today().strftime('%Y-%m-%d')}.")
-#     str_date = pyip.inputDatetime("What year do you want to travel to? Type in the date in this format YYYY-MM-DD: ",
-#                                   formats=('%Y-%m-%d',))
-#     date = str_date.date()
+str_date = pyip.inputDatetime("What year do you want to travel to? Type in the date in this format YYYY-MM-DD: ",
+                              formats=('%Y-%m-%d',))
+date = str_date.date()
 
-# hot_100 = requests.get(f"https://www.billboard.com/charts/hot-100/{str_date}")
-# hot_100.raise_for_status()
+while date > dt.datetime.today().date():
+    print(f"This is a date in the future, songs are not created yet for this date!\n"
+          f"Enter a date before or on {dt.datetime.today().strftime('%Y-%m-%d')}.")
+    str_date = pyip.inputDatetime("What year do you want to travel to? Type in the date in this format YYYY-MM-DD: ",
+                                  formats=('%Y-%m-%d',))
+    date = str_date.date()
+
+hot_100 = requests.get(f"https://www.billboard.com/charts/hot-100/{str_date.strftime('%Y-%m-%d')}")
+hot_100.raise_for_status()
 # Remember to wait for 5 secs between requests.
-with open('website.htm', encoding='utf-8') as file:
-    web_doc = file.read()
-# songs = BeautifulSoup(hot_100, features="html.parser")
-songs = BeautifulSoup(web_doc, features="html.parser")
+# with open('website.htm', encoding='utf-8') as file:
+#     web_doc = file.read()
+songs = BeautifulSoup(hot_100.text, features="html.parser")
+# songs = BeautifulSoup(web_doc, features="html.parser")
 song_list = songs.findAll('div', class_='o-chart-results-list-row-container')
 song_titles = []
 song_artists = []
@@ -37,7 +38,7 @@ for song in song_list:
         song_titles.append(h3_title)
         span_element = h3_element.find_next_sibling('span')  # Find the following span element
         if span_element:
-            span_text = span_element.getText(strip=True)  # Extract the text content of the span element
+            span_text = span_element.getText(strip=True).split()[0]  # Extract the text content of the span element
             song_artists.append(span_text)
 
 print(song_titles)
@@ -55,3 +56,13 @@ spotify = spotipy.Spotify(
 
 user_id = spotify.current_user()["id"]
 print(user_id)
+
+for idx in range(100):
+    result = spotify.search(q=f"{song_titles[idx]} artist: {song_artists[idx]} year:{str_date.strftime('%Y')}",
+                            limit=1, type="track", market="US")
+    try:
+        uri = result["tracks"]["items"][0]["uri"]
+    except IndexError:
+        print(f"{song_titles[idx]} doesn't exist in spotify.")
+    print(result)
+    sleep(1)
